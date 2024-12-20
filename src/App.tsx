53bf51766c6043485c4f012d8b49f20a01984464
Jon33260 <j.mallet82@laposte.net>
2024-12-20 09:15:11 +0100

    quete7

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,51 @@
 import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
-  const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
-
-    const [pokemonIndex, setPokemonIndex] = useState(0);
-    const pokemonSuivant = () => {
-      if (pokemonIndex < pokemonList.length - 1) {
-        setPokemonIndex(pokemonIndex + 1);
-      }
-    };
-
-    const pokemonPrecedent = () => {
-      pokemonIndex === 0 ? setPokemonIndex(0) : setPokemonIndex(pokemonIndex -1)
-    }
-
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
   return (
     <div>
-      <div>
-        <PokemonCard pokemon={pokemonList[pokemonIndex]} />
-      </div>
-      <div>
-        <button type="button" onClick={pokemonPrecedent}>
-          Précédent
-        </button>
-        <button type="button" onClick={pokemonSuivant}>
-          Suivant
-        </button>
-      </div>
-     
+      <nav>
+        {pokemonList.map((pokemon, i) => (
+          <button
+            type="button"
+            key={pokemon.name}
+            pokemon={pokemon.name}
+            onClick={() => setPokemonIndex(i)}
+          >
+            {pokemon.name}
+          </button>
+        ))}
+      </nav>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
   );
- 
 }
 export default App;
